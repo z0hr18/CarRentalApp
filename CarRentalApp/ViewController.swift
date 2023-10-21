b345facd9add03ccf59a64c9e5bbcdce0001c07b
Zohra <guliyevaazohra@gmail.com>
2023-10-21 12:15:33 +0400

    - project created

---
+++ CarRentalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarRentalApp
+//
+//  Created by Samxal Quliyev  on 21.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
